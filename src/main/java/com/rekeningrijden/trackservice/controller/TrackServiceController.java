4e2de9edb67f03b778fcf2f0f69ee27c8c240f26
Nick Litjens <i412205@fhict.nl>
2022-11-16 15:58:11 +0100

    pijn

--- src/main/java/com/rekeningrijden/trackservice/controller/TrackServiceController.java
+++ src/main/java/com/rekeningrijden/trackservice/controller/TrackServiceController.java
@@ -4,10 +4,8 @@ import com.rekeningrijden.trackservice.repository.DataPointRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("/")
 public class TrackServiceController {
@@ -24,5 +22,10 @@ public class TrackServiceController {
         catch (Exception e){
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping()
+    public ResponseEntity<DataPoint> StoreDataPoint() {
+        return new ResponseEntity<>(null, HttpStatus.FOUND);
     }
 }
